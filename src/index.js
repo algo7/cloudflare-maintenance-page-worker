4e38df45a10bc71071f5eee02990eb10a4354457
algo7 <11154774+algo7@users.noreply.github.com>
2024-03-11 02:59:54 +0100

    Add logging and update maintenance page

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ export default {
 			});
 
 			console.log('Client IP:', request.headers.get('cf-connecting-ip'));
+			console.log('Client Country:', request.headers.get('cf-ipcountry'));
+			console.log('Client City:', request.cf.city);
+			console.log('Client Region:', request.cf.region);
+			console.log('Backend Status:', backendResponse.status);
 
 			// Backend is up
 			if (backendResponse.ok) {
@@ -45,16 +49,24 @@ export default {
 
 function serveStatusPage() {
 	return new Response(`
-	  <!DOCTYPE html>
-	  <html>
-	  <head>
-		<title>Service Temporarily Unavailable</title>
-	  </head>
-	  <body>
-		<h1>Our service is currently down for maintenance.</h1>
-		<p>We apologize for the inconvenience and expect to be back online shortly.</p>
-	  </body>
-	  </html>
+	<!doctype html>
+	<title>Site Maintenance</title>
+	<style>
+	  body { text-align: center; padding: 150px; }
+	  h1 { font-size: 50px; }
+	  body { font: 20px Helvetica, sans-serif; color: #333; }
+	  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+	  a { color: #dc8100; text-decoration: none; }
+	  a:hover { color: #333; text-decoration: none; }
+	</style>
+	
+	<article>
+		<h1>We&rsquo;ll be back soon!</h1>
+		<div>
+			<p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:support@algo7.tools">contact me</a>, otherwise we&rsquo;ll be back online shortly!</p>
+			<p>&mdash; Algo7</p>
+		</div>
+	</article>
 	`, {
 		status: 503,
 		headers: { 'Content-Type': 'text/html' }
