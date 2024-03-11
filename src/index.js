20cbbf7befbdd2611b251a31e321e148bd551933
algo7 <11154774+algo7@users.noreply.github.com>
2024-03-11 03:26:45 +0100

    update maintenace page message

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ function serveStatusPage(contactEmail) {
 	<article>
 		<h1>We&rsquo;ll be back soon!</h1>
 		<div>
-			<p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:${contactEmail}">contact me</a>, otherwise we&rsquo;ll be back online shortly!</p>
+			<p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:${contactEmail}">contact me</a>, otherwise the service be back online shortly!</p>
 			<p>&mdash; Algo7</p>
 		</div>
 	</article>
